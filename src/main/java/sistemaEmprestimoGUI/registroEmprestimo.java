75ca49ca1581c3f006bae8c8d81ab0dc5b4aaad9
Dudah <uni.mariaeduardassf@gmail.com>
2024-05-23 22:48:43 -0300

    Fiz a tela de gerenciamento de ferramentas

--- src/main/java/sistemaEmprestimoGUI/registroEmprestimo.java
+++ src/main/java/sistemaEmprestimoGUI/registroEmprestimo.java
@@ -206,6 +206,11 @@ public class registroEmprestimo extends javax.swing.JFrame {
         jMenu2.add(jMenuItem1);
 
         jMenuItem3.setText("Gerenciar Ferramentas");
+        jMenuItem3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem3ActionPerformed(evt);
+            }
+        });
         jMenu2.add(jMenuItem3);
 
         jMenuBar1.add(jMenu2);
@@ -280,6 +285,11 @@ public class registroEmprestimo extends javax.swing.JFrame {
         // TODO add your handling code here:
         new gerenciarAmigo().setVisible(true);
     }//GEN-LAST:event_jMenuItem1ActionPerformed
+
+    private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
+        // TODO add your handling code here:
+        new gerenciarFerramentas().setVisible(true);
+    }//GEN-LAST:event_jMenuItem3ActionPerformed
 
     /**
      * @param args the command line arguments
